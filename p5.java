d653b14f727d7d7974a9cbcc975c40f9f7bec3ee
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-08 23:08:55 +0530

    p5 part1

---
+++ p5.java
@@ -0,0 +1,8 @@
+public class p5 {
+    public static void main(String[] args) {
+        double F = 2.3;
+        double C;
+        C = F-3.2/1.8;
+        System.out.println("Equation = "+C);
+    }
+}
