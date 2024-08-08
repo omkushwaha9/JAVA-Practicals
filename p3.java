12a8064d8bdd968120c32c4aae3c7417bad9d637
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-08 22:50:06 +0530

    Practical - 3

---
+++ p3.java
@@ -0,0 +1,8 @@
+public class p3 {
+ public static void main(String[] args) 
+ {
+ System.out.println("*******************"); 
+ System.out.println("*Let us learn JAVA*"); 
+ System.out.println("*******************");   
+ }    
+}
