906f0e0dca057d3bde360501cc12214904025fa7
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-09 21:06:23 +0530

    P7 Table Done

---
+++ p7.java
@@ -0,0 +1,10 @@
+public class p7 {
+    public static void main(String[] args) {
+        int N = 2;
+        int i = 1;
+        do{
+            System.out.println(N+"*"+i+"="+N*i);
+            i++;
+        } while (i<=10000);
+    }
+}
