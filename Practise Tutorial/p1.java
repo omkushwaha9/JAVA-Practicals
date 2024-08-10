be26a3511227c70c3e8c0e691e1abe37a4e2ef3f
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-10 14:24:49 +0530

    Practise Tutorial

---
+++ Practise Tutorial/p1.java
@@ -0,0 +1,13 @@
+public class p1 {
+   
+        public static void main(String[] args) {
+          int i = 0;
+          do {
+            System.out.println(i);
+            i++;
+          }
+          while (i < 99);  
+        }
+      }
+      
+
