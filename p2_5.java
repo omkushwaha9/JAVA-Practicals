5acee12b18e4abca4eafa58e80d64e09c5878014
omkushwaha9 <omkushwaha2005@gmail.com>
2024-08-22 16:43:03 +0530

    P2_5 Completed

---
+++ p2_5.java
@@ -0,0 +1,25 @@
+// Write a Program to list Prime Numbers 
+public class p2_5 {
+    static void prime_N(int N) {
+        int x, y, flg;
+        System.out.println(
+                "All the Prime numbers within 1 and " + N
+                        + " are:");
+        for (x = 2; x <= N; x++) {
+            flg = 1;
+            for (y = 2; y * y <= x; y++) {
+                if (x % y == 0) {
+                    flg = 0;
+                    break;
+                }
+            }
+            if (flg == 1)
+                System.out.print(x + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int N = 1000;
+        prime_N(N);
+    }
+}
