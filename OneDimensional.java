abfda3f8eed5ed889f1bf9725de770aceb9b5215
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-04 09:57:08 +0530

    OneDimensional Array

---
+++ OneDimensional.java
@@ -0,0 +1,13 @@
+public class OneDimensional
+{
+    public static void main(String[] args) {
+    int[] a=new int[3];
+    a[0]=10;
+    a[1]=20;
+    a[2]=30;
+    System.out.println("One Dimensional Array Element are: ");
+    System.out.println(a[0]);
+    System.out.println(a[1]);
+    System.out.println(a[2]);
+    }
+}
