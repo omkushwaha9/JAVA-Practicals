7c601788ca2bb00e04830a5649cf8bc310fb7064
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-04 13:29:34 +0530

    MultiDimensional Array

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,10 @@
+public class MultiDimensionalArray {
+    public static void main(String[] args) {
+    int[][] a={{10,20},{30,40}};
+    System.out.println("Multidimensional Array are");
+    System.out.println(a[0][0]);
+    System.out.println(a[0][1]); 
+    System.out.println(a[1][0]);
+    System.out.println(a[1][1]);
+    }
+}
