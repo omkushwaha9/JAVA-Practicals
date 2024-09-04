39748d6c1896e622d50ea2f7bd9eeada842704dd
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-04 10:07:41 +0530

    Formation

--- OneDimensional.java
+++ OneDimensional.java
@@ -1,13 +1,12 @@
-public class OneDimensional
-{
+public class OneDimensional {
     public static void main(String[] args) {
-    int[] a=new int[3];
-    a[0]=10;
-    a[1]=20;
-    a[2]=30;
-    System.out.println("One Dimensional Array Element are: ");
-    System.out.println(a[0]);
-    System.out.println(a[1]);
-    System.out.println(a[2]);
+        int[] a = new int[3];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        System.out.println("One Dimensional Array Element are: ");
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
     }
 }
