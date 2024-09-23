e6827cd454a5ee2a073c5a06e7bcfbcdb2504b31
omkushwaha9 <omkushwaha2005@gmail.com>
2024-09-23 16:37:45 +0530

    P5_1

---
+++ P5_1.java
@@ -0,0 +1,35 @@
+class Room {
+    double length;
+    double breadth;
+
+    public Room(double length, double breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public double area() {
+        return length * breadth;
+    }
+}
+
+class BedRoom extends Room {
+    double height;
+
+    public BedRoom(double length, double breadth, double height) {
+        super(length, breadth); 
+        this.height = height;     
+            }
+
+    public double volume() {
+        return length * breadth * height;   
+        }
+}
+
+public class P5_1 {
+    public static void main(String[] args) {
+        BedRoom bedroom = new BedRoom(10.5, 8.2, 3.0);
+
+        System.out.println("Area of the bedroom: " + bedroom.area()); 
+        System.out.println("Volume of the bedroom: " + bedroom.volume());
+    }
+}
