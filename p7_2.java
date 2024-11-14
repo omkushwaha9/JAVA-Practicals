a1d4382e06a48909379e894a1c0ba67aff77c0aa
omkushwaha9 <omkushwaha2005@gmail.com>
2024-11-14 17:05:22 +0530

    P7_2 Code Completion

---
+++ p7_2.java
@@ -0,0 +1,25 @@
+class MyRunnable implements Runnable {
+    private String threadName;
+
+    public MyRunnable(String name) {
+        this.threadName = name;
+    }
+
+    
+    public void run() {
+        System.out.println(threadName + " is running.");
+    }
+}
+
+public class p7_2 {
+    public static void main(String[] args) {
+        
+        Thread thread1 = new Thread(new MyRunnable("Thread 1"));
+        Thread thread2 = new Thread(new MyRunnable("Thread 2"));
+        Thread thread3 = new Thread(new MyRunnable("Thread 3"));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
