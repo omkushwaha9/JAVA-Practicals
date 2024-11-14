c1d4d812bca0585a6bd3c7906a5ca7ca3133a4b2
omkushwaha9 <omkushwaha2005@gmail.com>
2024-11-14 16:32:58 +0530

    Formation

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 public class Main {
     int modelYear;
     String modelName;
-  
+
     public Main(int year, String name) {
-      modelYear = year;
-      modelName = name;
+        modelYear = year;
+        modelName = name;
     }
-  
+
     public static void main(String[] args) {
-      Main myCar = new Main(1969, "Mustang");
-      System.out.println(myCar.modelYear + " " + myCar.modelName);
+        Main myCar = new Main(1969, "Mustang");
+        System.out.println(myCar.modelYear + " " + myCar.modelName);
     }
-  }
-  
+}
