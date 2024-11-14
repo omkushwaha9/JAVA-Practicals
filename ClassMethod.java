ebfe3d4fc966ad760dd591024090fcb486f2119d
omkushwaha9 <omkushwaha2005@gmail.com>
2024-11-14 16:39:47 +0530

    Class Method

---
+++ ClassMethod.java
@@ -0,0 +1,10 @@
+public class Main {
+    static void myMethod() {
+      System.out.println("Hello World!");
+    }
+  
+    public static void main(String[] args) {
+      myMethod();
+    }
+  }
+  
