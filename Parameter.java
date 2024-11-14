8e795cfcf609a17e5f7ebfe2b5aeb3b889c8bbf4
omkushwaha9 <omkushwaha2005@gmail.com>
2024-11-14 16:34:38 +0530

    Formation and Compilation

--- Parameter.java
+++ Parameter.java
@@ -1,9 +1,9 @@
 public class Parameter {
     static void myMethod(int x) {
         System.out.println(x);
-      }
-    
-      public static void main(String[] args) {
+    }
+
+    public static void main(String[] args) {
         myMethod(10);
-      }
+    }
 }
