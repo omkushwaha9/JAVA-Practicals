bd2bbc188405d73ebca27081fd0357ea45feef90
omkushwaha9 <omkushwaha2005@gmail.com>
2024-11-14 16:44:21 +0530

    Formation

--- PublicAttributes.java
+++ PublicAttributes.java
@@ -1,9 +1,8 @@
 class MyClass {
     public static void main(String[] args) {
-      Person myObj = new Person(); 
-      System.out.println("Name: " + myObj.fname + " " + myObj.lname);
-      System.out.println("Email: " + myObj.email);
-      System.out.println("Age: " + myObj.age);
+        Person myObj = new Person();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("Email: " + myObj.email);
+        System.out.println("Age: " + myObj.age);
     }
-  }
-  
+}
